8e070c1b48746c152bfe2c5a27f0b4c3ed94c4c3
yerko <yerko.franulic.v@gmail.com>
2024-01-30 23:10:49 -0300

    Se agregan validaciones de Template

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-template',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './template.component.css'
 })
 export class TemplateComponent {
+
+  usuario = {
+    nombre: 'Yerko',
+    apellido: 'Franulic',
+    correo: 'yerko.franulic.v@gmail.com'
+  }
+
+  guardar(forma: NgForm) {
+    console.log(forma);
+    console.log(forma.value);
+  }
 
 }
